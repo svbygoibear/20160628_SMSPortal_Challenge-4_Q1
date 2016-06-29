eebee8047e067a9588caa30d9425d501df3bd27f
simoneslatnpou <svbyogibear@gmail.com>
2016-06-29 22:32:03 +0200

    Initial commit.

---
+++ 20160628_SMSPortal_Challenge-4_Q1/MathUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SMSPortal_Challenge4 {
+    class MathUtils {
+        public static double Average(double a, double b) {
+            return (a + b) / 2;
+        }
+
+        public static void Main(string[] args) {
+            Console.WriteLine(Average(2, 1));
+            Console.ReadLine();
+        }
+    }
+}
